db071f0f80fc0f001747b891cb192166f855f960
Kevin Harrington <mad.hephaestus@gmail.com>
2018-01-03 23:30:57 -0500

    Updated to faster slicer

--- SliceTest.groovy
+++ SliceTest.groovy
@@ -7,6 +7,44 @@ import javax.vecmath.Matrix4d;
 import eu.mihosoft.vrl.v3d.ext.org.poly2tri.DelaunayTriangle;
 import eu.mihosoft.vrl.v3d.ext.org.poly2tri.PolygonUtil;
 import com.neuronrobotics.bowlerstudio.threed.Line3D;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import com.neuronrobotics.bowlerstudio.assets.AssetFactory;
+import javax.imageio.ImageIO;
+import com.neuronrobotics.bowlerstudio.utils.ImageTracer;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.imageio.ImageIO;
+
+import com.neuronrobotics.nrconsole.util.FileSelectionFactory;
+import com.neuronrobotics.sdk.common.Log;
+import eu.mihosoft.vrl.v3d.CSG;
+import eu.mihosoft.vrl.v3d.Cube;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.MeshView;
+import javafx.scene.transform.Scale;
+import javafx.stage.Stage;
 
 
 println "Loading slicer"
@@ -267,6 +305,7 @@ ISlice se = new ISlice (){
 					.toZMin();
 			// Loop over each polygon in the slice of the incoming CSG
 			// Add the polygon to the final slice if it lies entirely in the z plane
+			println "Preparing CSG slice"
 			for(Polygon p: incoming
 					.transformed(slicePlane)
 					.intersect(planeCSG)						
@@ -276,9 +315,9 @@ ISlice se = new ISlice (){
 				}
 			}
 			
-			
+			println "Fixing edges"
 			List<Polygon> fixed = updateEdges( rawPolygons)	
-	
+			println "Triangulating"
 			List<Polygon> triangles =[]
 			ArrayList<Edge> allEdges = []
 			for (int i = 0; i < fixed.size(); i++) {
@@ -287,10 +326,12 @@ ISlice se = new ISlice (){
 				for(List<Edge> newTri:triangleEdges){
 					allEdges.addAll(newTri)
 				}
-			}							
+			}	
+			println "Prune to unique edges only"						
 			ArrayList<Edge> finalEdges=uniqueOnly(allEdges)
 
 			//println "New edges = "+finalEdges.size()+" to "+allEdges.size()
+			println "form boundary Paths"		
 	         List<Polygon>boundaryPaths =  boundaryPaths(finalEdges)
 	         //println "Boundary paths = "+boundaryPaths.size()
 	         //List<Polygon> parts= Edge.boundaryPathsWithHoles(boundaryPaths);       		
@@ -816,10 +857,304 @@ ISlice se = new ISlice (){
 
 	};
 
-Slice.setSliceEngine(se)
+ISlice se2 =new ISlice (){
+	int xPix
+	int yPix
+	int toPix(int x,int y){
+		return ((x)*(yPix+2))+y+1
+	}
+	   /**
+     * Determines whether the specified point lies on tthis edge.
+     *
+     * @param p point to check
+     * @param TOL tolerance
+     * @return <code>true</code> if the specified point lies on this line
+     * segment; <code>false</code> otherwise
+     */
+    public boolean containsPoint(Vector3d p,Vertex p1 ,Vertex p2,double TOL) {
+
+  		double x = p.x;
+        double x1 = p1.pos.x;
+        double x2 = p2.pos.x;
+
+        double y = p.y;
+        double y1 = p1.pos.y;
+        double y2 = p2.pos.y;
+
+        double AB = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) );
+        double AP = Math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1) );
+        double PB = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y));
+
+        return Math.abs(AB - (AP + PB)) < TOL;
+    }
+        /**
+     * Contains.
+     *
+     * @param p the p
+     * @return true, if successful
+     */
+    public boolean polygonContains(Vector3d p,Polygon poly) {
+        // taken from http://www.java-gaming.org/index.php?topic=26013.0
+        // and http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+        double px = p.x;
+        double py = p.y;
+        boolean oddNodes = false;
+        double x2 = poly.vertices.get(poly.vertices.size() - 1).pos.x;
+        double y2 = poly.vertices.get(poly.vertices.size() - 1).pos.y;
+        double x1, y1;
+        for (int i = 0; i < poly.vertices.size();  ++i) {
+        	Vertex v1 = poly.vertices.get(i)
+		Vertex v2
+		if(i<poly.vertices.size()-1){
+			v2=poly.vertices.get(i+1)
+		}else
+			v2=poly.vertices.get(0)
+		
+	     if (containsPoint(p,v1,v2,0.0000001)) {
+	         return true;
+	     }
+            x1 = poly.vertices.get(i).pos.x;
+            y1 = poly.vertices.get(i).pos.y;
+            if (((y1 < py) && (y2 >= py))
+                    || (y1 >= py) && (y2 < py)) {
+                if ((py - y1) / (y2 - y1)
+                        * (x2 - x1) < (px - x1)) {
+                    oddNodes = !oddNodes;
+                }
+            }
+            x2 = x1
+            y2 = y1
+            
+        }
+        return oddNodes;
+    }
+/**
+	 * An interface for slicking CSG objects into lists of points that can be extruded back out
+	 * @param incoming			  Incoming CSG to be sliced
+	 * @param slicePlane		  Z coordinate of incoming CSG to slice at
+	 * @param normalInsetDistance Inset for sliced output
+	 * @return					  A set of polygons defining the sliced shape
+	 */
+	List<Polygon> slice(CSG incoming, Transform slicePlane, double normalInsetDistance){
+		List<Polygon> rawPolygons = new ArrayList<>();
+
+		// Actual slice plane
+		CSG planeCSG = incoming.getBoundingBox()
+				.toZMin();
+		// Loop over each polygon in the slice of the incoming CSG
+		// Add the polygon to the final slice if it lies entirely in the z plane
+		println "Preparing CSG slice"
+		CSG slicePart =incoming
+				.transformed(slicePlane)
+				.intersect(planeCSG)
+		for(Polygon p: slicePart						
+				.getPolygons()){
+			if(Slice.isPolygonAtZero(p)){
+				rawPolygons.add(p);
+			}
+		}
+		//BowlerStudioController.getBowlerStudio() .addObject((Object)slicePart.movez(1),(File)null)
+		//BowlerStudioController.getBowlerStudio() .addObject((Object)rawPolygons,(File)null)
+		double ratio = slicePart.getTotalY()/slicePart.getTotalX() 
+		
+		LengthParameter printerOffset 			= new LengthParameter("printerOffset",0.5,[1.2,0])
+		double scalePixel = 0.25
+		double size = slicePart.getTotalX()/printerOffset.getMM()/scalePixel
+		if(slicePart.getTotalY()>slicePart.getTotalX() ){
+			size = slicePart.getTotalY()/printerOffset.getMM()/scalePixel
+			ratio = slicePart.getTotalX()/slicePart.getTotalY()
+		}
+		xPix = size*(ratio>1?1.0:ratio);
+		yPix = size*(ratio<1?1.0:ratio);
+		int pixels = (xPix+2)*(yPix+2)
+		double xOffset = slicePart.getMinX()
+		double yOffset = slicePart.getMinY()
+		double scale = slicePart.getTotalX()/xPix
+		
+		boolean [] pix =new boolean [pixels]
+		println "Image x=" +xPix+" by y="+yPix+" at x="+xOffset+" y="+yOffset
+		long start = System.currentTimeMillis()
+		int imageOffset =20
+		WritableImage obj_img = new WritableImage(xPix+imageOffset, yPix+imageOffset);
+		//int snWidth = (int) 4096;
+		//int snHeight = (int) 4096;
+
+		MeshView sliceMesh = slicePart.getMesh();
+		sliceMesh.getTransforms().add(javafx.scene.transform.Transform.translate(2, 2));
+		AnchorPane anchor = new AnchorPane(sliceMesh);
+		AnchorPane.setBottomAnchor(sliceMesh, (double) 0);
+		AnchorPane.setTopAnchor(sliceMesh, (double) 0);
+		AnchorPane.setLeftAnchor(sliceMesh, (double) 0);
+		AnchorPane.setRightAnchor(sliceMesh, (double) 0);
+		snapshotGroup = new Pane(anchor);
+		snapshotGroup.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+
+
+		SnapshotParameters snapshotParameters = new SnapshotParameters();
+		snapshotParameters.setTransform(new Scale(1/scale, 1/scale));
+		snapshotParameters.setDepthBuffer(true);
+		snapshotParameters.setFill(Color.TRANSPARENT);
+		
+		Runnable r =new Runnable() {
+			boolean done =false
+			@Override
+			public void run() {
+				snapshotGroup.snapshot(snapshotParameters, obj_img);
+				done=true
+			}
+		}
+		Platform.runLater(r)
+		while(r.done== false){
+			Thread.sleep(10)
+		}
+		
+        	byte alpha = (byte) 0;
+		for(int i=0;i<xPix;i++){
+			for(int j=0;j<yPix;j++){
+				int color = obj_img.getPixelReader().getArgb(i, j);
+				if((color & 0xff000000)>0)
+					pix[toPix(i,j)]=true;
+				else
+					pix[toPix(i,j)]=false;
+			}
+		}
+		println "Find boundries "+((double)(System.currentTimeMillis()-start)/1000.0)
+		
+	
+		
+		ImageView sliceImage = new ImageView(obj_img);
+		
+		sliceImage.getTransforms().add(javafx.scene.transform.Transform.translate(xOffset, xOffset));
+		sliceImage.getTransforms().add(javafx.scene.transform.Transform.scale(scale,scale ));
+		BowlerStudioController.getBowlerStudio() .addNode(sliceImage)
+		//
+
+		double MMTOPX = 3.5409643774783404;
+		float outputScale = (float) (MMTOPX / scale);
+		// Options
+		HashMap<String, Float> options = new HashMap<String, Float>();
+
+		// Tracing
+		options.put("ltres", 1f);// Error treshold for
+									// straight lines.
+		options.put("qtres", 1f);// Error treshold for
+									// quadratic splines.
+		options.put("pathomit", 0.02f);// Edge node paths
+									// shorter than this
+									// will be discarded for
+									// noise reduction.
+
+		// Color quantization
+		options.put("colorsampling", 1f); // 1f means true ;
+											// 0f means
+											// false:
+											// starting with
+											// generated
+											// palette
+		options.put("numberofcolors", 16f);// Number of
+											// colors to use
+											// on palette if
+											// pal object is
+											// not defined.
+		options.put("mincolorratio", 0.02f);// Color
+											// quantization
+											// will
+											// randomize a
+											// color if
+											// fewer pixels
+											// than (total
+											// pixels*mincolorratio)
+											// has it.
+		options.put("colorquantcycles", 1f);// Color
+											// quantization
+											// will be
+											// repeated this
+											// many times.
+		//
+		// SVG rendering
+		options.put("scale", outputScale);// Every
+											// coordinate
+											// will be
+											// multiplied
+											// with this, to
+											// scale the
+											// SVG.
+		options.put("simplifytolerance", 1f);//
+		options.put("roundcoords", 2f); // 1f means rounded
+										// to 1 decimal
+										// places, like 7.3
+										// ; 3f means
+										// rounded to 3
+										// places, like
+										// 7.356 ; etc.
+		options.put("lcpr", 0f);// Straight line control
+								// point radius, if this is
+								// greater than zero, small
+								// circles will be drawn in
+								// the SVG. Do not use this
+								// for big/complex images.
+		options.put("qcpr",0f);// Quadratic spline control
+								// point radius, if this is
+								// greater than zero, small
+								// circles and lines will be
+								// drawn in the SVG. Do not
+								// use this for big/complex
+								// images.
+		options.put("desc", 0f); // 1f means true ; 0f means
+									// false: SVG
+									// descriptions
+									// deactivated
+		options.put("viewbox", 1f); // 1f means true ; 0f
+									// means false: fixed
+									// width and height
+
+		// Selective Gauss Blur
+		options.put("blurradius", 0f); // 0f means
+										// deactivated; 1f
+										// .. 5f : blur with
+										// this radius
+		options.put("blurdelta", 20f); // smaller than this
+									// RGB difference
+									// will be blurred
+		print "\nTracing..."
+		BufferedImage bi = SwingFXUtils.fromFXImage(obj_img,(BufferedImage)null)
+		String svg = com.neuronrobotics.bowlerstudio.utils.ImageTracer.imageToSVG(bi,options,(byte[][])null)
+		int headerStart = svg.indexOf(">")+1
+		int headerEnd = svg.lastIndexOf("<")
+		println "headerStart "+headerStart+ " headerEnd "+headerEnd
+		String header = svg.substring(0,headerStart)
+		String footer = svg.substring(headerEnd,svg.size())
+		String body = svg.substring(headerStart,headerEnd)
+		body = "<g id=\"g37\">\n"+body+"</g>\n"
+		svg=header+body+footer
+		//println header+"\n\n"
+		//println body+"\n\n"
+		//println footer+"\n\n"
+		File tmpsvg = new File( System.getProperty("java.io.tmpdir")+"/"+Math.random())
+		tmpsvg.createNewFile()
+		FileWriter fw = new FileWriter(tmpsvg.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(svg);
+		bw.close();
+		Transform tr = new Transform()
+					.translate(xOffset, xOffset,0)
+					.scale(scale/28.3)
+		List<Polygon>  svgPolys = SVGLoad.toPolygons(tmpsvg).collect{
+			it.transform(tr)
+		}
+		//tmpsvg.delete()
+		print "Done Slicing!\n"
+		svgPolys.remove(0)
+		//println svg
+		BowlerStudioController.getBowlerStudio() .addObject((Object)svgPolys,(File)null)
+		return 	svgPolys
+	}
+}
+Slice.setSliceEngine(se2)
 
 if(args != null)
  return
+BowlerStudioController.getBowlerStudio().getJfx3dmanager().clearUserNode() 
 // Create a CSG to slice
 CSG pin = new Cylinder(10, 100)
 	.toCSG()
@@ -838,9 +1173,13 @@ CSG carrot = new Cylinder(100,  10)
 	cubePin.movey(-60)
 	]
 	)
+	//.movex(200)
+	//.movey(200)
 	//.roty(30)
 	//.rotx(30)
-	
-Transform slicePlane = new Transform()
 
-return [Slice.slice(carrot.prepForManufacturing(),slicePlane, 0),carrot]
+Transform slicePlane = new Transform()
+//Image ruler = AssetFactory.loadAsset("BowlerStudio-Icon.png");
+//ImageView rulerImage = new ImageView(ruler);
+Slice.slice(carrot.prepForManufacturing(),slicePlane, 0)
+return null
